12e41671060231215fc2a3f1501f93c12e1c6dae
jonmlas <jonmlas@gmail.com>
2025-08-21 18:52:35 +0800

    Manifest updated

--- manifest.php
+++ manifest.php
@@ -2,10 +2,28 @@
 
 $manifest = array();
 
-$manifest['name'] = __( 'Builder', 'fw' );
-$manifest['version'] = '1.2.12';
+$manifest['name']        = __( 'Builder', 'fw' );
+$manifest['slug']        = 'unyson-builder';
+$manifest['description'] = __( 'Unyson Page Builder Extension', 'fw' );
+
+$manifest['version']     = '1.2.12';
+$manifest['display']     = true;
+$manifest['standalone']  = true;
+
+// Repository Info
 $manifest['github_update'] = 'ThemeFuse/Unyson-Builder-Extension';
-$manifest['github_repo'] = 'https://github.com/ThemeFuse/Unyson-Builder-Extension';
-$manifest['uri'] = 'http://manual.unyson.io/en/latest/extension/builder/index.html';
-$manifest['author'] = 'ThemeFuse';
+$manifest['github_repo']   = 'https://github.com/ThemeFuse/Unyson-Builder-Extension';
+$manifest['github_branch'] = 'master';
+
+// Author Info
+$manifest['author']     = 'ThemeFuse';
 $manifest['author_uri'] = 'http://themefuse.com/';
+
+// Meta
+$manifest['license']      = 'GPL-2.0-or-later';
+$manifest['text_domain']  = 'fw';
+$manifest['requires_php'] = '7.4';
+$manifest['requires_wp']  = '5.8';
+
+// Documentation
+$manifest['uri'] = 'http://manual.unyson.io/en/latest/extension/builder/index.html';
