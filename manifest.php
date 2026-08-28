5259f1a18448ced4fc9e44713b380d4bbf02dd60
jonmlas <jonmlas@gmail.com>
2026-08-28 22:01:44 +0800

    Version bump

--- manifest.php
+++ manifest.php
@@ -5,7 +5,7 @@ $manifest = array();
 $manifest['name']        = __( 'Builder', 'fw' );
 $manifest['description'] = __( 'Unyson Page Builder Extension', 'fw' );
 
-$manifest['version']     = '1.2.83';
+$manifest['version']     = '1.2.84';
 
 // Repository Info
 $manifest['github_update'] = 'UnysonPlus/UnysonPlus-Builder-Extension';
